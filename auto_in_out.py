72801f73ba9505b1ef92f4f720884153337811ee
Lee <cico1379@gmail.com>
2021-02-16 22:48:27 +0900

    'in_out_func_success'

--- auto_in_out.py
+++ auto_in_out.py
@@ -7,28 +7,38 @@ from datetime import datetime
 
 def send_slack_message(message):
     payload = '{"text": "%s"}' % message.encode('utf-8').decode('latin-1')
-    response = requests.post('https://hooks.slack.com/services/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',
+    response = requests.post('https://hooks.slack.com/services/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',
                              data = payload)
     print(response.text)
 
-# def main(argv):
-# 
-#     try: opts, args = getopt.getopt(argv, "hm:", ["message="])
-# 
-#     except getopt.GetoptError:
-#         print('SlackMessage.py -m <message>')
-#         sys.exit(2)
-#     if len(opts) == 0:
-#         message = "HELLO, WORLD!"
-#     for opt, arg in opts:
-#         if opt == '-h':
-#             print('SlackMessage.py -m <message>')
-#             sys.exit()
-#         elif opt in ("-m", "--message"):
-#             message = arg
-# 
-#     send_slack_message(message)
+def main(argv):
+
+    date = datetime.strftime(datetime.now(), '%Y-%m-%d %H:%M')
+
+    message = ' '
+    try:
+        opts, args = getopt.getopt(argv, "hm:", ["message="])
+
+    except getopt.GetoptError:
+        print('SlackMessage.py -m <message>')
+        sys.exit(2)
+
+    if len(opts) == 0:
+        message = "HELLO, WORLD!"
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print('SlackMessage.py -m <message>')
+            sys.exit()
+        elif opt in ("-m", "--message"):
+            if arg == 'in':
+                message = date + ' ' + '출근'
+            elif arg == 'out':
+                message = date + ' ' + '퇴근'
+            else :
+                print("input 'in' or 'out'")
+
+    send_slack_message(message)
 
 if __name__ == "__main__":
-    date = datetime.now()
-    send_slack_message(f"{datetime.strftime(date, '%Y-%m-%d %H:%M')} 출근")
+    main(sys.argv[1:])
